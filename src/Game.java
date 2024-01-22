eb711f346260734bbfc1cc1c0600ce75a8a72076
NonsensicalQuagga <94972468+NonsensicalQuagga@users.noreply.github.com>
2024-01-22 13:49:34 +0100

    optimised SemisolidPlatform

--- src/Game.java
+++ src/Game.java
@@ -39,26 +39,7 @@ public class Game extends Engine {
         }
         player.drawPlayer(g);
         }
-/*
-    public void platformCollision(Player player, Platform platform){
-        int[] platformPosition = platform.getPlatformCollision();
-        int[] playerPosition = player.getPosition();
-        int playerSpeedX = player.getSpeedX();
-        int playerSpeedY = player.getSpeedY();
-             if ( playerPosition[0] + playerSpeedX <= platformPosition[0] + platformPosition[2] &&
-                     playerPosition[0] + playerPosition[2] + playerSpeedX >= platformPosition[0] &&
-                     playerPosition[1] + playerSpeedY <= platformPosition[1] + platformPosition[3] &&
-                     playerPosition[1] + playerPosition[3] + playerSpeedY >= platformPosition[1]) {
-             boolean collidingTop = (playerPosition[1] + playerPosition[3] <= platformPosition[1] && playerPosition[1] + playerPosition[3] + playerSpeedY >= platformPosition[1]);
-             if (collidingTop){
-             player.setSpeedY(0);
-             player.setGrounded(true);
-             player.setPositionY(platformPosition[1] - playerPosition[3]);
-            }
 
-
-        }
-    }*/
 }
 
 
--- src/SemisolidPlatform.java
+++ src/SemisolidPlatform.java
@@ -16,9 +16,9 @@ public class SemisolidPlatform extends Platform {
         if ( playerPosition[0] + playerSpeedX <= platformPosition[0] + platformPosition[2] &&
                 playerPosition[0] + playerPosition[2] + playerSpeedX >= platformPosition[0] &&
                 playerPosition[1] + playerSpeedY <= platformPosition[1] + platformPosition[3] &&
+                playerPosition[1] + playerPosition[3] + playerSpeedY >= platformPosition[1] &&
+                playerPosition[1] + playerPosition[3] <= platformPosition[1] &&
                 playerPosition[1] + playerPosition[3] + playerSpeedY >= platformPosition[1]) {
-            boolean collidingTop = (playerPosition[1] + playerPosition[3] <= platformPosition[1] && playerPosition[1] + playerPosition[3] + playerSpeedY >= platformPosition[1]);
-            if (collidingTop){
                 player.setSpeedY(0);
                 player.setGrounded(true);
                 player.setPositionY(platformPosition[1] - playerPosition[3]);
@@ -26,5 +26,28 @@ public class SemisolidPlatform extends Platform {
 
 
         }
-    }
+        /*
+
+        Old version of semisolid
+
+    public void platformCollision(Player player, Platform platform){
+        int[] platformPosition = platform.getPlatformCollision();
+        int[] playerPosition = player.getPosition();
+        int playerSpeedX = player.getSpeedX();
+        int playerSpeedY = player.getSpeedY();
+             if ( playerPosition[0] + playerSpeedX <= platformPosition[0] + platformPosition[2] &&
+                     playerPosition[0] + playerPosition[2] + playerSpeedX >= platformPosition[0] &&
+                     playerPosition[1] + playerSpeedY <= platformPosition[1] + platformPosition[3] &&
+                     playerPosition[1] + playerPosition[3] + playerSpeedY >= platformPosition[1]) {
+             boolean collidingTop = (playerPosition[1] + playerPosition[3] <= platformPosition[1] && playerPosition[1] + playerPosition[3] + playerSpeedY >= platformPosition[1]);
+             if (collidingTop){
+             player.setSpeedY(0);
+             player.setGrounded(true);
+             player.setPositionY(platformPosition[1] - playerPosition[3]);
+            }
+
+
+        }
+    }*/
+
 }
