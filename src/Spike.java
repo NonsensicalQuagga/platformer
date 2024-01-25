a99df394d37ea607e9a279a46cfb040243d0f17c
NonsensicalQuagga <94972468+NonsensicalQuagga@users.noreply.github.com>
2024-01-25 13:11:25 +0100

    slightly changed spike behaviour

--- src/Spike.java
+++ src/Spike.java
@@ -27,28 +27,31 @@ public class Spike extends Platform {
         int[] playerPosition = player.getPosition();
         int playerSpeedX = player.getSpeedX();
         int playerSpeedY = player.getSpeedY();
-        if (playerPosition[0] + playerSpeedX <= platformPosition[0] + platformPosition[2] &&
-                playerPosition[0] + playerPosition[2] + playerSpeedX >= platformPosition[0] &&
-                playerPosition[1] + playerSpeedY <= platformPosition[1] + platformPosition[3] &&
-                playerPosition[1] + playerPosition[3] + playerSpeedY >= platformPosition[1]) {
-            boolean collidingTop = (playerPosition[1] + playerPosition[3] <= platformPosition[1] && playerPosition[1] + playerPosition[3] + playerSpeedY >= platformPosition[1]);
-            boolean collidingBot = (playerPosition[1] >= platformPosition[1] + platformPosition[3] && playerPosition[1] + playerSpeedY <= platformPosition[1] + platformPosition[3]);
-            boolean collidingLeft = (playerPosition[0] + playerPosition[2] <= platformPosition[0] && playerPosition[0] + playerPosition[2] + playerSpeedX >= platformPosition[0]);
-            boolean collidingRight = (playerPosition[0] >= platformPosition[0] + platformPosition[2] && playerPosition[0] + playerSpeedX <= platformPosition[0] + platformPosition[2]);
-            if (collisionType == 1){
+        if (playerPosition[0] <= platformPosition[0] + platformPosition[2] &&
+                playerPosition[0] + playerPosition[2] >= platformPosition[0] &&
+                playerPosition[1] <= platformPosition[1] + platformPosition[3] &&
+                playerPosition[1] + playerPosition[3] >= platformPosition[1]) {
+            if (collisionType == 1) {
                 player.die();
-            }
-            if (collisionType == 2 && ((collidingRight || collidingTop || collidingBot) || playerSpeedX <= 0) ){
-                // no collision if player moves right through the block
-                player.die();
-            }
-            if (collisionType == 3 && ((collidingLeft || collidingTop || collidingBot) || playerSpeedX >= 0)){
-                // no collision if player moves left through the block
-                player.die();
-            }
-            if (collisionType == 4 && ((collidingLeft || collidingTop || collidingRight) || playerSpeedY >= 0)){
-                // no collision if player comes from bellow the block
-                player.die();
+            } else {
+
+                boolean collidingTop = (playerPosition[1] + playerPosition[3] <= platformPosition[1] && playerPosition[1] + playerPosition[3] + playerSpeedY >= platformPosition[1]);
+                boolean collidingBot = (playerPosition[1] >= platformPosition[1] + platformPosition[3] && playerPosition[1] + playerSpeedY <= platformPosition[1] + platformPosition[3]);
+                boolean collidingLeft = (playerPosition[0] + playerPosition[2] <= platformPosition[0] && playerPosition[0] + playerPosition[2] + playerSpeedX >= platformPosition[0]);
+                boolean collidingRight = (playerPosition[0] >= platformPosition[0] + platformPosition[2] && playerPosition[0] + playerSpeedX <= platformPosition[0] + platformPosition[2]);
+
+                if (collisionType == 2 && ((collidingRight || collidingTop || collidingBot) || playerSpeedX <= 0)) {
+                    // no collision if player moves right through the block
+                    player.die();
+                }
+                if (collisionType == 3 && ((collidingLeft || collidingTop || collidingBot) || playerSpeedX >= 0)) {
+                    // no collision if player moves left through the block
+                    player.die();
+                }
+                if (collisionType == 4 && ((collidingLeft || collidingTop || collidingRight) || playerSpeedY >= 0)) {
+                    // no collision if player comes from bellow the block
+                    player.die();
+                }
             }
 
         }
